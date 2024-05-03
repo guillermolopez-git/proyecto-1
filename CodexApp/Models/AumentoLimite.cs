6be625794c5fe65441891eee87db8818dc249e6b
guillermolopez-git <168255088+guillermolopez-git@users.noreply.github.com>
2024-05-03 04:44:15 -0600

    Delete CodexApp directory

--- CodexApp/Models/AumentoLimite.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CodexApp.Models
-{
-    public class AumentoLimite(int pin, int nuevoLimite)
-    {
-        public int Pin { get; set; } = pin;
-        public int NuevoLimite { get; set; } = nuevoLimite;
-    }
-}
--- CodexApp/Models/Events/LimiteUpdateEvent.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CodexApp.Models.Events
-{
-    public class LimiteUpdateEvent: EventArgs // eventargs : clase base para clases que contienen datos de eventos
-    {
-        public int Pin { get; set; }
-
-        public int UpdatedLimite { get; set; }
-    }
-}
